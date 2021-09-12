1a14485e67066a9e45a7adaad6c92878971716e7
sewon <shin6403@gmail.com>
2021-09-12 20:19:06 +0900

    wip

---
+++ src/components/Ckeditor.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { storage } from "../firebase";
+
+class MyUploadAdapter {
+  constructor(loader) {
+    this.loader = loader;
+  }
+  // Starts the upload process.
+  upload() {
+    return this.loader.file.then(
+      (file) =>
+        new Promise((resolve, reject) => {
+          let uploadTask = storage.ref(`images/${file.name}`).put(file);
+          uploadTask.on(
+            "state_changed", // or 'state_changed'
+            function (snapshot) {
+              console.log("snapshot.state11111>>>>>>>>", snapshot.state);
+              // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+              const progress =
+                (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+              // console.log("Upload is " + progress + "% done");
+              switch (snapshot.state) {
+                case "paused": // or 'paused'
+                  // console.log("Upload is paused");
+                  console.log("snapshot.state22222>>>>>>>>", snapshot.state);
+                  break;
+                case "running": // or 'running'
+                  // console.log("Upload is running");
+                  console.log("snapshot.state3333333>>>>>>>>", snapshot.state);
+                  break;
+              }
+            },
+            function (error) {
+              // A full list of error codes is available at
+              // https://firebase.google.com/docs/storage/web/handle-errors
+              // eslint-disable-next-line default-case
+              switch (error.code) {
+                case "storage/unauthorized":
+                  reject(" User doesn't have permission to access the object");
+                  break;
+
+                case "storage/canceled":
+                  reject("User canceled the upload");
+                  break;
+
+                case "storage/unknown":
+                  reject(
+                    "Unknown error occurred, inspect error.serverResponse"
+                  );
+                  break;
+              }
+            },
+            function () {
+              // Upload completed successfully, now we can get the download URL
+              uploadTask.snapshot.ref
+                .getDownloadURL()
+                .then(function (downloadURL) {
+                  // console.log("File available at", downloadURL);
+                  resolve({
+                    default: downloadURL,
+                  });
+                });
+            }
+          );
+        })
+    );
+  }
+}
+
+function Ckeditor(props) {
+  const [data, setData] = useState("");
+  const [tagData, setTagData] = useState(null);
+
+  const handleOnchange = (_, editor) => {
+    const data = editor.getData();
+    setTagData(data);
+  };
+
+  return (
+    <>
+      <CKEditor
+        editor={ClassicEditor}
+        data={data}
+        onChange={handleOnchange}
+        onReady={(editor) => {
+          editor.plugins.get("FileRepository").createUploadAdapter = (
+            loader
+          ) => {
+            return new MyUploadAdapter(loader);
+          };
+        }}
+      />
+      {tagData}
+    </>
+  );
+}
+
+export default Ckeditor;
---
+++ src/components/FirebaseImgUpload.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { storage } from "../firebase";
+
+function FirebaseImgUpload(props) {
+  // console.log("storage>>>", storage);
+  const [image, setImage] = useState(null);
+  const [url, setUrl] = useState("");
+  const [progress, setProgress] = useState(0);
+
+  console.log("image>>>", image);
+
+  const handleChange = (e) => {
+    if (e.target.files[0]) {
+      setImage(e.target.files[0]);
+    }
+  };
+
+  const handleUpload = () => {
+    const uploadTask = storage.ref(`images/${image.name}`).put(image);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        console.log("Upload is " + progress + "% done");
+        setProgress(progress);
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        storage
+          .ref("images")
+          .child(image.name)
+          .getDownloadURL()
+          .then((url) => {
+            setUrl(url);
+          });
+      }
+    );
+  };
+
+  return (
+    <div>
+      firebase img
+      <progress value={progress} max={100} />
+      <br />
+      <input type="file" onChange={handleChange} />
+      <button onClick={handleUpload}>Upload</button>
+      <br />
+      {url}
+      <br />
+      <img
+        src={url || "http://via.placeholder.com/300x300"}
+        alt="firebase-image"
+      />
+    </div>
+  );
+}
+
+export default FirebaseImgUpload;
